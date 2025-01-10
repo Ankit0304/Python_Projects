17f3d8f9e6cc28c77af6627386aab4fccb19a054
Ankit Baingane <ankitbaingane157@gmail.com>
2025-01-10 19:36:12 +0530

    Add Comments

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,16 +1,19 @@
+# Rock Paper Scissors Game 
 import random
 
 while True:
+    
     print("Welcome to Rock, Paper, Scissors Game")
     print("Choose one of the following:")
     print('''1. Rock
 2. Paper
 3. Scissors''')
+    # Taking user input
     user=int(input("Enter your choice: "))
     while user>3 or user<1:
         print("Invalid input! Please enter a valid input")
         user=int(input("Enter valid input: "))
-    
+    # Display user choice 
     if user==1:
             user_choice='Rock'
     elif user==2:
@@ -19,8 +22,12 @@ while True:
             user_choice='Scissors'
             
     print(f"Your choice is: {user_choice}")
+    
+    # Random Computer choice
     print("Now it's computer's turn")
     computer=random.randint(1,3)
+    
+    # Display computer choice
     if computer==1:
         computer_choice='Rock'
     elif computer==2:
@@ -30,6 +37,7 @@ while True:
         
     print(f"Computer's choice is: {computer_choice}")
     
+    # Compare user and computer choice and declare winner
     if user==computer:
         print("It's a tie!")
     elif user==1 and computer==2 or user==2 and computer==3 or user==3 and computer==1:
